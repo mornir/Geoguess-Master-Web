3953ca144aba74cd13c83c0f70f0dfd33c87d4d5
mornir <jeromepott@hotmail.com>
2019-12-20 09:54:42 +0100

    Remove appId

--- src/main.js
+++ src/main.js
@@ -30,7 +30,6 @@ var firebaseConfig = {
   projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.VUE_APP_FIREBASE_PROJECT_ID + ".appspot.com",
   messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.VUE_APP_FIREBASE_APP_ID,
   measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
 }
 // Initialize Firebase
